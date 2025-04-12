96b66de29305da0b24a08743cd44e69d8e0543a0
Richard Zhu <rz262@cornell.edu>
2025-04-12 14:34:31 -0400

    path fix attempt 2

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,10 @@
 import os
 
+# Path to config
+CURR_PATH = os.path.dirname(os.path.abspath(__file__))
+
 # Set root path
-ROOT_PATH = os.path.abspath(os.path.join("..", os.curdir))
+ROOT_PATH = os.path.abspath(os.path.join(CURR_PATH, ".."))
 
 # Data file path
 DATA_PATH = os.path.join(os.path.dirname(__file__), "data", "profs_and_publications.json")
